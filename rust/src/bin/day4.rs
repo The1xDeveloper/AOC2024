98f3a1f7cdf2f2447c280adeaccac99c598c0dab
The1xDeveloper <The1xDeveloper@gmail.com>
2024-12-04 15:57:20 -0500

    Day4 rust, minor tweeks playing around with gpt solution to understand more

--- rust/src/bin/day4.rs
+++ rust/src/bin/day4.rs
@@ -13,6 +13,9 @@ fn main() {
     part1(&lines);
     part2(&lines);
 }
+fn is_in_bounds(xi: i32, yi: i32, max_x: i32, max_y: i32) -> bool {
+    (0 <= xi && xi <= max_x) && (0 <= yi && yi <= max_y)
+}
 fn find(xi: i32, yi: i32, lines: &[String]) -> i32 {
     let directions = [
         (1, 0),
@@ -51,14 +54,11 @@ fn find(xi: i32, yi: i32, lines: &[String]) -> i32 {
 fn find2(xi: i32, yi: i32, lines: &[String]) -> i32 {
     let max_y = lines.len() as i32 - 1;
     let max_x = lines[0].len() as i32 - 1;
+    if !(is_in_bounds(xi + 1, yi + 1, max_x, max_y) && is_in_bounds(xi - 1, yi - 1, max_x, max_y)) {
+        return 0;
+    }
 
-    if !(((0 <= (xi + 1)) && ((xi + 1) <= max_x)) && ((0 <= (xi - 1)) && ((xi - 1) <= max_x))) {
-        return 0;
-    }
-    if !(((0 <= (yi + 1)) && ((yi + 1) <= max_y)) && ((0 <= (yi - 1)) && ((yi - 1) <= max_y))) {
-        return 0;
-    }
-    let mut diagonals = vec![];
+    let mut diagonals = Vec::with_capacity(2);
 
     diagonals.push((
         lines[(yi - 1) as usize]
