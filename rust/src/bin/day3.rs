4333bb09b1bc2f9cca3363771d8e25c3b0599bd3
The1xDeveloper <The1xDeveloper@gmail.com>
2024-12-08 11:13:12 -0500

    Day3, rust. Optimized with gpt help

--- rust/src/bin/day3.rs
+++ rust/src/bin/day3.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use regex::Regex;
 use std::cmp;
 use std::collections::HashMap;
@@ -12,20 +13,77 @@ fn main() {
         .map_while(Result::ok)
         .collect();
     part1(&lines);
+    part2(&lines);
+}
+fn find_all(s: &str, sub: &str, start: i32) -> Vec<i32> {
+    let idx_o = s[(start as usize)..].find(sub).map(|i| i as i32 + start);
+    if idx_o.is_none() {
+        return Vec::new();
+    }
+    let idx = idx_o.unwrap();
+    let more = find_all(s, sub, idx + 1);
+
+    let mut rtn = Vec::new();
+    rtn.push(idx);
+    if !more.is_empty() {
+        rtn.extend(more.iter());
+    }
+    rtn
+}
+fn find_closest(idx: i32, nums: &[i32]) -> i32 {
+    nums.iter()
+        .filter_map(|x| {
+            let t = idx - x;
+            if t > 0 {
+                Some(t)
+            } else {
+                None
+            }
+        })
+        .min()
+        .unwrap_or(99999999)
 }
 
-fn test_nums(nums: Vec<i32>) -> bool {
-    let window: Vec<i32> = nums.windows(2).map(|a| a[0] - a[1]).collect();
-    let is_mono = window.iter().all(|&a| a > 0);
-    let is_mono_dec = window.iter().all(|&a| a < 0);
-    let is_in_bounds = window.iter().all(|&a| 1 <= a.abs() && a.abs() <= 3);
-    (is_mono || is_mono_dec) && is_in_bounds
+fn part2(lines: &[String]) {
+    let full_string: String = lines.par_iter().cloned().collect();
+    let mut potentials: Vec<(i32, i32)> = Vec::new();
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let potentials: Vec<(i32, i32)> = lines
+        .par_iter()
+        .flat_map(|line| {
+            re.captures_iter(line)
+                .map(|c| (c[1].parse::<i32>().unwrap(), c[2].parse::<i32>().unwrap()))
+                .collect::<Vec<_>>()
+        })
+        .collect();
+    let mut dos: Vec<i32> = Vec::new();
+    dos.push(0);
+    let t = find_all(full_string.as_str(), "do()", 0);
+    if !t.is_empty() {
+        dos.extend(t.iter());
+    }
+    let donts = find_all(full_string.as_str(), "don't()", 0);
+    let ans: i32 = potentials
+        .par_iter()
+        .filter_map(|(x, y)| {
+            let idx = full_string.find(format!("mul({x},{y}").as_str()).unwrap() as i32;
+            let closest_do = find_closest(idx, &dos);
+            let closest_dont = find_closest(idx, &donts);
+            if closest_do < closest_dont {
+                Some(x * y)
+            } else {
+                None
+            }
+        })
+        .sum();
+
+    println!("pt2: {}", ans);
 }
 fn part1(lines: &[String]) {
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
     let ans: i32 = lines
-        .iter()
+        .par_iter()
         .map(|line| {
-            let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
             re.captures_iter(line)
                 .map(|c| c[1].parse::<i32>().unwrap() * c[2].parse::<i32>().unwrap())
                 .sum::<i32>()
