492b45150a851c99a5e4df827a48091d6ddbe626
The1xDeveloper <The1xDeveloper@gmail.com>
2024-12-09 10:52:18 -0500

    Rename dir structure for sorting

---
+++ python/day10/day10.py
@@ -0,0 +1,29 @@
+import re
+import heapq
+from collections import deque, defaultdict, Counter
+
+
+input = open("../../inputs/day10.txt").read().splitlines()
+
+
+def is_in_bounds(point, max_x, max_y):
+    return (0 <= point[0] <= max_x) and (0 <= point[1] <= max_y)
+
+
+def part1b(lines):
+    ans = 0
+    for yi, line in enumerate(lines):
+        pass
+    return ans
+
+
+def part2(lines):
+    ans = 0
+    for yi, line in enumerate(lines):
+        pass
+    return ans
+
+
+print(part1b(input))
+print("***********part 2***************")
+print(part2(input))
