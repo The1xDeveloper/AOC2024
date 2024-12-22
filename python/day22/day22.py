2b40c87d47411be522de0818ae85b99ceea31765
The1xDeveloper <The1xDeveloper@gmail.com>
2024-12-22 08:52:57 -0500

    Day22, Python added alternative code with bit wise operations

--- python/day22/day22.py
+++ python/day22/day22.py
@@ -41,6 +41,25 @@ def play2(number, time, t = None, c = Counter()):
         t.append(s)
     return play2(number, time - 1, t, c)
 
+def play3(number, time, t = None, c = Counter()):
+    if time == 0:
+        return c
+    orig = number
+    number = ((number << 6) ^ number) & 0xFFFFFF
+    number = ((number >> 5) ^ number) & 0xFFFFFF
+    number = ((number << 11) ^ number) & 0xFFFFFF
+    s = number % 10 - orig % 10
+    if t and len(t) == 4:
+        k = (t[0], t[1], t[2], t[3])
+        if k not in c:
+            c[k] = orig % 10
+        t.popleft()
+    if t is None:
+        t = deque([s])
+    else:
+        t.append(s)
+    return play3(number, time - 1, t, c)
+
 def play(number, time):
     if time == 0:
         return number
@@ -58,9 +77,18 @@ def part1(lines):
 
 def part2(lines):
     all = Counter()
+    s = time.time()
     for yi, line in enumerate(lines):
         all += play2(int(line), 2000, None, Counter())
+    print("Original took: ", time.time() - s)
+    s = time.time()
+    all2 = Counter()
+    for yi, line in enumerate(lines):
+        all2 += play3(int(line), 2000, None, Counter())
+    print("mod 10 took: ", time.time() - s)
 
+    print(all.most_common(1)[0][1])
+    print(all2.most_common(1)[0][1])
     return all.most_common(1)[0][1]
 
 print("ans pt1:", part1(i))
