15c01f80cdb032e515270cc09f76e247f048eb0e
The1xDeveloper <The1xDeveloper@gmail.com>
2025-01-15 11:31:58 -0500

    Add recursive solution for example

--- python/day11/day11.py
+++ python/day11/day11.py
@@ -1,6 +1,8 @@
 import re
+import time
 import heapq
 from collections import deque, defaultdict, Counter
+from functools import cache
 
 
 input = open("../../inputs/day11.txt").read().splitlines()
@@ -29,6 +31,20 @@ def part1(lines):
 
     return len(stones)
 
+@cache
+def dfs(stone, days):
+    if days == 0:
+        return 1
+    
+    if stone == 0:
+        return dfs(1, days - 1)
+    elif len(str(stone)) % 2 == 0:
+        str_stone = str(stone)
+        a = str_stone[: len(str_stone) // 2]
+        b = str_stone[len(str_stone) // 2 :]
+        return dfs(int(a), days - 1) + dfs(int(b), days - 1)
+    else:
+        return dfs(stone * 2024, days - 1)
 
 def part2(lines):
     stones = [int(x) for x in lines[0].split()]
@@ -50,6 +66,7 @@ def part2(lines):
                 new_m[stone * 2024] += num
         m = new_m
 
+    print(sum(dfs(x, 75) for x in stones))
     return sum(m.values())
 
 
